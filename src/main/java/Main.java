24c27b5b167599850567f7bc0a0527912c0da2a0
angelortizv <angelo2898@gmail.com>
2021-09-03 18:53:09 -0600

    Add prints for changes in branch feature/gradle-tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,7 +17,8 @@ public class Main {
             HttpGet request = new HttpGet("https://swapi.dev/api/people");
             CloseableHttpResponse response = httpClient.execute(request);
             try {
-                System.out.println("Hello World!");
+                System.out.println("Welcome to:");
+                System.out.println("Feature/gradle-test branch!");
                 System.out.println(response.getProtocolVersion());              // HTTP/1.1
                 System.out.println(response.getStatusLine().getStatusCode());   // 200
                 System.out.println(response.getStatusLine().getReasonPhrase()); // OK
